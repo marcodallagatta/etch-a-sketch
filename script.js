988830f5a73e83e21bb7861639ec28461ca4d467
Marco Dalla Gatta <marcodg@github.com>
2022-04-25 12:17:40 +0200

    complete project with reset button, new grid button, and opacity

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 const gridCont = document.getElementById('grid');
+const resetButton = document.getElementById('reset');
+const newGridButton = document.getElementById('newGrid');
 
 function gridMake(squares = 16) {
-    let div = document.createElement('div');
+    // resets the container grid
+    gridCont.innerHTML = '';
+    gridCont.style.gridTemplateColumns = '';
+    // crete divs for the container grid
+    const div = document.createElement('div');
     for (let i = 0; i < squares; i++) {
         gridCont.appendChild(div.cloneNode(div));
     }
-
+    // generated the correct number of '1fr' for the container grid
+    const sideNum = Math.sqrt(squares);
+    for (let x = 0; x < sideNum; x++) {
+        gridCont.style.gridTemplateColumns += ' 1fr';
+    }
+    // calls the event listeners for the new grid
+    hoverHighlight('black');
 }
-gridMake(16)
-
-const gridElems = document.querySelectorAll('#grid div');
+gridMake(100);
 
 function hoverHighlight(color = 'red') {
+    const gridElems = document.querySelectorAll('#grid div');
     gridElems.forEach(item => {
         item.addEventListener('mouseover', () => {
-            item.style.background = color;
+            if (item.style.opacity === '') {
+                let randomHexColor = Math.floor(Math.random()*16777215).toString(16);
+                item.style.background = `#${randomHexColor}`;
+                item.style.opacity = '0.2';
+            } else  if (item.style.opacity < 1) {
+                item.style.opacity = parseFloat(item.style.opacity) + 0.2;
+            }
         })
     });
 }
-hoverHighlight('black');
+
+function reset() {
+    const gridElems = document.querySelectorAll('#grid div');
+    gridElems.forEach(item => {
+        item.style.background = '';
+        item.style.opacity = '';
+    });
+}
+resetButton.addEventListener('click', function() {reset()});
+newGrid.addEventListener('click', function() {
+    const size = prompt('Input size for new grid, maximum is 100:');
+    if (size > 100 || isNaN(size)) {
+        alert('Invalid selection');
+    } else {
+        reset();
+        gridMake(size);
+    }
+});
